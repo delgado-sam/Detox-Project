41872c5600ec3375f9735ccc27a3ece8ba0c3d18
Sam Delgado <delgado.saam25@gmail.com>
2022-10-22 17:40:38 -0500

    detox

---
+++ android/app/build/generated/source/buildConfig/androidTest/debug/com/projectdetox/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.projectdetox.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.projectdetox.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+}
